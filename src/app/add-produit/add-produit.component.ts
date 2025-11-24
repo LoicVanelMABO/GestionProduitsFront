903197d244a0495b4f951bc78edd182bca127c27
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2025-11-24 23:44:46 +0100

    add produit & form add produit

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-add-produit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-produit.component.html',
   styleUrl: './add-produit.component.css'
 })
-export class AddProduitComponent {
+export class AddProduitComponent implements OnInit {
+  newProduit = new Produit;
+  message?:string;
 
+  constructor(private produitService : ProduitService){
+
+  }
+
+  ngOnInit():void{
+  }
+  
+  addProduit(){
+    alert("hello");
+    //console.log(this.newProduit);
+    this.produitService.ajouterProduit(this.newProduit);
+    this.message = "l'article "+this.newProduit.nomProduit+" a bien été ajouté."
+
+  }
 }
