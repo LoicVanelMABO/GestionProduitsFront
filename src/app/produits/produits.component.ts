2592966236499e63d855935f06763511b65f5e5d
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2025-11-24 23:44:46 +0100

    text commit add produit & form add produit
    :wq

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,18 +1,27 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+<<<<<<< HEAD
 import { ɵEmptyOutletComponent, RouterLink } from "@angular/router";
+=======
+import { ɵEmptyOutletComponent } from "@angular/router";
+>>>>>>> 903197d (add produit & form add produit)
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-produits',
   standalone: true,
+<<<<<<< HEAD
   imports: [NgFor, ɵEmptyOutletComponent, CommonModule, RouterLink],
+=======
+  imports: [NgFor, ɵEmptyOutletComponent,CommonModule],
+>>>>>>> 903197d (add produit & form add produit)
   templateUrl: './produits.component.html',
   styleUrl: './produits.component.css'
 })
 
 export class ProduitsComponent implements OnInit{
+<<<<<<< HEAD
   
   ModifierProduit(produit: Produit) {
     //this.produitService.suppProduit(produit);
@@ -24,6 +33,8 @@ export class ProduitsComponent implements OnInit{
     this.produitService.suppProduit(produit);
   }
 
+=======
+>>>>>>> 903197d (add produit & form add produit)
   produits? : Produit[]; //tableau de produit
 
   constructor(private produitService: ProduitService){
