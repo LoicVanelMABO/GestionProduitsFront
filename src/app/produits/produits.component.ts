b574d3dcca6604878c1ff051bafe32a45dcce2dc
Loïc Vanel MABO <85876405+LoicVanelMABO@users.noreply.github.com>
2026-01-21 03:15:54 +0100

    Sec 9 form add proct (#4)
    * add produit
    
    * add produit & form add produit
    
    * supp elt du tableau
    
    * modif produit

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,27 +1,29 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ɵEmptyOutletComponent } from "@angular/router";
+import { ɵEmptyOutletComponent, RouterLink } from "@angular/router";
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-produits',
   standalone: true,
-  imports: [NgFor, ɵEmptyOutletComponent,CommonModule],
+  imports: [NgFor, ɵEmptyOutletComponent, CommonModule, RouterLink],
   templateUrl: './produits.component.html',
   styleUrl: './produits.component.css'
 })
 
 export class ProduitsComponent implements OnInit{
-ModifierProduit(produit: Produit) {
-  this.produitService.suppProduit(produit);
+  
+  ModifierProduit(produit: Produit) {
+    //this.produitService.suppProduit(produit);
     alert('Modification du produit ' + produit.nomProduit);
-    this.produitService.modifierProduit(produit);
+    //this.produitService.modifierProduit(produit);
   }
 
   supprimerProduit(produit: Produit) {
     this.produitService.suppProduit(produit);
   }
+
   produits? : Produit[]; //tableau de produit
 
   constructor(private produitService: ProduitService){
