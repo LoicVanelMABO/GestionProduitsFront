600e67e4b72d48648638cd63d3798e2832f75aa3
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2026-01-21 14:13:11 +0100

    update product

---
+++ src/app/update-produit/update-produit.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ProduitService } from '../services/produit.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Produit } from '../model/produit.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-update-produit',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './update-produit.component.html',
+  styles: ``
+})
+export class UpdateProduitComponent implements OnInit {
+
+  
+  currentProduit!: Produit;
+  constructor(private produitService: ProduitService, private activatedRoute: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    let id = this.activatedRoute.snapshot.params['id']
+    this.currentProduit = this.produitService.consulterProduit(id);
+  }
+
+  updateProduit() {
+    this.produitService.updateProduit(this.currentProduit);
+    this.router.navigate(['produits']);
+  }
+
+}
