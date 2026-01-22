8b82079ea17c16a4e12a22fb2d5ab73a1b31e98f
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2026-01-22 22:57:11 +0100

    ajout model

--- src/app/model/produit.model.ts
+++ src/app/model/produit.model.ts
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
+
 import { Categorie } from "./categorie.model";
 
-=======
->>>>>>> 903197d (add produit & form add produit)
+
+
 export class Produit{
     idProduit? : number;
     nomProduit? : string;
     prixProduit? : number;
     dateCreation? : Date;
-<<<<<<< HEAD
+
     categorie? : Categorie;
-=======
->>>>>>> 903197d (add produit & form add produit)
+
+
 }
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,50 +1,27 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { ɵEmptyOutletComponent, RouterLink } from "@angular/router";
-=======
-import { ɵEmptyOutletComponent } from "@angular/router";
->>>>>>> 903197d (add produit & form add produit)
+import { RouterLink } from "@angular/router";
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-produits',
   standalone: true,
-<<<<<<< HEAD
-  imports: [NgFor, ɵEmptyOutletComponent, CommonModule, RouterLink],
-=======
-  imports: [NgFor, ɵEmptyOutletComponent,CommonModule],
->>>>>>> 903197d (add produit & form add produit)
+  imports: [NgFor, CommonModule, RouterLink],
   templateUrl: './produits.component.html',
   styleUrl: './produits.component.css'
 })
 
 export class ProduitsComponent implements OnInit{
-<<<<<<< HEAD
-<<<<<<< HEAD
   
   ModifierProduit(produit: Produit) {
-    //this.produitService.suppProduit(produit);
     alert('Modification du produit ' + produit.nomProduit);
-    //this.produitService.modifierProduit(produit);
-=======
-ModifierProduit(produit: Produit) {
-  this.produitService.suppProduit(produit);
-    alert('Modification du produit ' + produit.nomProduit);
-    this.produitService.modifierProduit(produit);
->>>>>>> 871cdbf (supp elt du tableau)
   }
 
   supprimerProduit(produit: Produit) {
     this.produitService.suppProduit(produit);
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> 903197d (add produit & form add produit)
-=======
->>>>>>> 871cdbf (supp elt du tableau)
   produits? : Produit[]; //tableau de produit
 
   constructor(private produitService: ProduitService){
